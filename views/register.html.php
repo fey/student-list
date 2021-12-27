133899d5095b0762cd14864d50da58853f031ea9
feycot <feycot@gmail.com>
2021-12-27 20:51:37 +0500

    fix register form

--- views/register.html.php
+++ views/register.html.php
@@ -3,31 +3,35 @@
     <h1 class="p-5">Registration</h1>
     <div class="card">
       <div class="card-body p-3">
-        <form>
+        <form action="/sign_up" method="POST">
           <div class="mb-3">
             <label for="user_first_name" class="form-label">First name</label>
-            <input type="text" class="form-control" id="user_first_name" aria-describedby="first_name_help">
+            <input type="text" name="user[first_name]" class="form-control" id="user_first_name" aria-describedby="first_name_help">
           </div>
           <div class="mb-3">
             <label for="user_last_name" class="form-label">Last name</label>
-            <input type="text" class="form-control" id="user_last_name" aria-describedby="first_name_help">
+            <input type="text" name="user[last_name]" class="form-control" id="user_last_name" aria-describedby="first_name_help">
           </div>
           <div class="mb-3">
             <label for="user_email" class="form-label">Email address</label>
             <input type="email" class="form-control" id="user_email" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <div id="emailHelp" name="user[email]" class="form-text">We'll never share your email with anyone else.</div>
           </div>
           <div class="mb-3">
             <label for="user_password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="user_password">
+            <input type="password" name="user[password]" class="form-control" id="user_password">
           </div>
           <div class="mb-3">
             <label for="user_group_id" class="form-label">Group ID</label>
-            <input type="text" class="form-control" id="user_group_id">
+            <input type="text" name="user[group_id]" class="form-control" id="user_group_id">
+          </div>
+          <div class="mb-3">
+            <label for="user_exam_points" class="form-label">Exam Points</label>
+            <input type="number" name="user[exam_points]" class="form-control" id="user_exam_points">
           </div>
           <div class="mb-3">
             <label for="user_birthday_date" class="form-label">Birthday Date</label>
-            <input type="date" class="form-control" id="user_birthday_date" max="2005-01-01">
+            <input type="date" name="user[birthday]" class="form-control" id="user_birthday_date" max="2005-01-01">
           </div>
           <div class="form-check">
             <input class="form-check-input" type="radio" name="user[gender]" id="user_gender1" checked>
